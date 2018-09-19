907903f49ad09aa4b186ca5e39e6eec1a2f293bb
Daniel <dcastillo3@gmail.com>
2018-09-19 17:19:31 -0400

    test: succesfully tested datepicker library

--- src/components/CustomerInfo.jsx
+++ src/components/CustomerInfo.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';
 
 export default class CustomerInfo extends Component {
   constructor(props) {
@@ -8,12 +11,19 @@ export default class CustomerInfo extends Component {
       firstName: "",
       lastName: "",
       phoneNumber: "",
-      email: ""
+      email: "",
+      startDate: moment()
     }
   }
 
   handleChange = evt => {
     this.setState({[evt.target.name]: evt.target.value});
+  }
+
+  handleDate = date => {
+    this.setState({
+      startDate: date
+    });
   }
 
   handleSubmit = evt => {
@@ -25,8 +35,9 @@ export default class CustomerInfo extends Component {
   }
 
   render() {
+    console.log(this.state.startDate.format())
     return (
-      <div>
+      <div className="test-form">
         <form onSubmit={this.handleSubmit}>
           <label>First Name:</label>
           <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
@@ -40,6 +51,21 @@ export default class CustomerInfo extends Component {
           <label>Email:</label>
           <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
           <br></br>
+          <label>Date:</label>
+          <DatePicker
+            todayButton={"Today"}
+            selected={this.state.startDate}
+            onChange={this.handleDate}
+            showTimeSelect
+            timeFormat="HH:mm"
+            timeIntervals={30}
+            dateFormat="LLL"
+            timeCaption="time"
+            minTime={moment().hours(10).minutes(0)}
+            maxTime={moment().hours(18).minutes(0)}
+            minDate={moment()}
+            maxDate={moment().add(15, "days")}
+           />
           <button>Submit</button>
         </form>
       </div>
