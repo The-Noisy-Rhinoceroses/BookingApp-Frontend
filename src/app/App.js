cae39d2988b852acfe41423cdbb764b0d9860288
Daniel Castillo <dcastillo3@gmail.com>
2018-09-26 02:50:32 -0400

    chore(App.js): migrated appointment form into modal

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { CustomerInfo, AllBarbersContainer } from '../components';
 import './App.css';
+import Modal from 'react-responsive-modal';
 
 // TODO: Relocate the CustomerInfo Component later on;
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modalOpen: false
+    }
+  }
+
+  //following two functions for opening/closing modal on local state
+  onOpenModal = () => {
+    this.setState({ modalOpen: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ modalOpen: false });
+  };
+
   render() {
     return (
       <div className="App">
-        <CustomerInfo />
         <AllBarbersContainer />
+        <Modal open={this.state.modalOpen} onClose={this.onCloseModal} center>
+          <CustomerInfo />
+        </Modal>
+        <button onClick={this.onOpenModal}>Open Appointment Form</button>
       </div>
     );
   }
