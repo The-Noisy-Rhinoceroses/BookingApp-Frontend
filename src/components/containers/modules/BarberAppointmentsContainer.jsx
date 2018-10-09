20465e6c77cd8234645dbe9568783488b4b1e41c
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-09 17:48:01 -0400

    refactor(BarberAppointmentsContainer): Remove button to force re-render

--- src/components/containers/modules/BarberAppointmentsContainer.jsx
+++ src/components/containers/modules/BarberAppointmentsContainer.jsx
@@ -4,27 +4,14 @@ import { fetchBookedAppointments } from '../../../thunks';
 import { BarberAppointmentsView } from '../../views/modules'
 
 class BarberAppointmentsContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      click: false
-    }
-  }
-
-  handleClick = () => {
-    console.log("hiii from container", this.state)
-    this.setState({click: true}); // TODO: REMOVE THIS;
+  componentDidMount() {
     this.props.listAppointments(this.props.currentBarber._id);
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>Reset everything</button>
-        {this.state.click
-        ? <BarberAppointmentsView appointments={this.props.appointments} handleClick={this.handleClick} clickState={this.state.click} />
-        : <p>Hahahaha</p>
-        }
+        <BarberAppointmentsView appointments={this.props.appointments} />
       </div>
     )
   }
