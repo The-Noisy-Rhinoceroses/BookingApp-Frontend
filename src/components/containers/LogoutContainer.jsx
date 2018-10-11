385152f9cbb8fee8faa1d9e782c2ae9fc64da14f
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-11 17:07:14 -0400

    feat(Login/Logout): Push users to home or dashboard if they logout or login respectively

--- src/components/containers/LogoutContainer.jsx
+++ src/components/containers/LogoutContainer.jsx
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { LogoutView } from '../views';
 import { logout } from '../../thunks';
+import { withRouter } from 'react-router-dom';
 
 class LogoutContainer extends Component {
   handleClick = evt => {
     evt.preventDefault();
     this.props.logout();
+    this.props.history.push('/home')
   };
 
   render() {
     return <LogoutView handleClick={this.handleClick} />
   }
-};
+}
 
 const mapDispatch = dispatch => {
   return {
@@ -25,7 +27,7 @@ LogoutContainer.propTypes = {
   logout: PropTypes.func.isRequired
 };
 
-export default connect(
+export default withRouter(connect(
   null,
   mapDispatch
-)(LogoutContainer);
+)(LogoutContainer));
