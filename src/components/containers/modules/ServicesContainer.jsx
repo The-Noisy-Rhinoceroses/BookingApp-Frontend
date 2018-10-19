3b7b14d5f881afbd0ffc328c72693cc4922db9df
Richard Lanchez <richardsunny2015@gmail.com>
2018-10-19 12:13:38 -0400

    Select and unselect services #81 (#82)
    * test(services): Attempt to remove from selected Services
    
    * style(ServicesContainer): Use hasOwnProperty and service variable
    
    * refactor(ServicesContainer): Modify appropriate properties of the state
    
    * feat(ServicesContainer): Send the service object as a value for it's relevant input

--- src/components/containers/modules/ServicesContainer.jsx
+++ src/components/containers/modules/ServicesContainer.jsx
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { ServicesView } from '../../views/modules/'
+import ServicesView  from '../../views/modules/ServicesView';
 
 class ServicesContainer extends Component {
   constructor() {
     super();
     this.state = {
-      services: []
+      services: [],
+      selectedServices: {},
+    }
+  }
+
+  handleChange = (evt) => {
+    let service = JSON.parse(evt.target.value);
+    if (this.state.selectedServices.hasOwnProperty(service.serviceName)) {
+      const currentlySelectedServices = {...this.state.selectedServices};
+      delete currentlySelectedServices[service.serviceName];
+      this.setState({selectedServices: currentlySelectedServices});
+    }
+    else {
+      this.setState({selectedServices: {...this.state.selectedServices, [service.serviceName]: service}})
     }
   }
 
   componentDidMount() {
     axios
-      .get("/api/services")
+      .get('/api/services')
       .then(res => res.data)
       .then(services => this.setState({services}))
       .catch(err => console.log(err))
   }
 
   render() {
-    return <ServicesView services={this.state.services} />
+    console.log(this.state);
+    return <ServicesView services={this.state.services} handleChange={this.handleChange} />
   }
 }
 
