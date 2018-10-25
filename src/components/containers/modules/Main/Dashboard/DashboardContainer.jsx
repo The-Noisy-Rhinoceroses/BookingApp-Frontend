2ddd7f6a47147c0e74618b5d659fd3d361956f60
richardsunny2015 <richardsunny2015@gmail.com>
2018-10-25 16:33:12 -0400

    fix(Dashboard): Fix appointments handlers for customers and barbers

--- src/components/containers/modules/Main/Dashboard/DashboardContainer.jsx
+++ src/components/containers/modules/Main/Dashboard/DashboardContainer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchBookedAppointments } from '../../../../../thunks';
+import { fetchCoworkerAppointments } from '../../../../../thunks';
 import { DashboardView } from '../../../../views/modules/Main';
 
 class DashboardContainer extends Component {
@@ -22,7 +22,7 @@ class DashboardContainer extends Component {
 const mapState = state => {
   return {
     currentBarber: state.currentUser,
-    appointments: state.bookedAppointments.sort((a, b) => {
+    appointments: state.coworkerAppointments.sort((a, b) => {
       if (a.date === b.date) return 0;
       return a.date > b.date ? 1 : -1; // sorts strings of dates in ascending order
     })
@@ -31,7 +31,7 @@ const mapState = state => {
 
 const mapDispatch = dispatch => {
   return {
-    listAppointments: barberId => dispatch(fetchBookedAppointments(barberId))
+    listAppointments: barberId => dispatch(fetchCoworkerAppointments(barberId))
   };
 };
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,3 +4,4 @@ export { default as currentBarber } from '../store/utilities/currentBarber';
 export { default as bookedAppointments } from '../store/utilities/bookedAppointments';
 export { default as currentUser } from '../store/utilities/currentUser';
 export { default as selectedServices } from '../store/utilities/selectedServices';
+export { default as coworkerAppointments } from '../store/utilities/coworkerAppointments';
