ff0eda90c6e680581eecc3fb9e832d08ee974f71
Allan James Lapid <ajLapid718@users.noreply.github.com>
2018-10-24 20:57:44 -0400

    feat(DashboardMenu): Connect dashboard menu to store and filter appointments by barber (#99)

--- src/components/views/modules/Main/Dashboard/DashboardMenuView.jsx
+++ src/components/views/modules/Main/Dashboard/DashboardMenuView.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-
 const DashboardMenuView = props => {
-  
+  const { allBarbers, handleChange, currentUser } = props;
   return (
     <div className="dashboard-menu">
-        This is the Dasboard Menu.
+      This is the Dashboard Menu.
+      <select name="selectedBarber" onChange={handleChange}>
+        {allBarbers.map(barber => (
+          <option selected={currentUser._id === barber._id} value={barber._id}>{barber.firstName} {barber.lastName}</option>
+        ))}
+      </select>
     </div>
-  )
+  );
 };
 
 export default DashboardMenuView;
