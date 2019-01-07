847c914052e2136255347f35652c396558e87f59
Daniel <dcastillo3@gmail.com>
2019-01-06 23:12:32 -0500

    Migrate(DashboardMenuView.jsx): Migrate dashboard menu

--- src/components/views/modules/Main/Dashboard/DashboardMenuView.jsx
+++ src/components/views/modules/Main/Dashboard/DashboardMenuView.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 
 const DashboardMenuView = props => {
-  const { allBarbers, handleChange, selectedUser } = props;
+  const { allBarbers, handleClick, selectedUser } = props;
+
   return (
     <div className="dashboard-menu">
-      This is the Dashboard Menu.
+      {/* This is the Dashboard Menu.
       <select name="selectedBarber" onChange={handleChange} value={selectedUser}>
         {allBarbers.map(barber => (
           <option key={barber._id} value={barber._id}>{barber.firstName} {barber.lastName}</option>
         ))}
-      </select>
+      </select> */}
+
+      <div className="dashboard-barber-grid flex-column text-center">
+          {allBarbers.map(barber => {
+            const active = selectedUser === barber._id ? 'active' : '';
+            const bgImage = {
+              backgroundImage: `url(${barber.imgUrl})`
+            }
+
+            return (
+              <div key={barber._id} onClick={(event) => handleClick(event)} value={barber._id} className={`dashboard-barber-container flex-column flex-center small-padding ${active}`}>
+                {/* <div style={bgImage} className="dashboard-barber-photo"></div> */}
+                <i class="dashboard-barber-icon far fa-address-card"></i>
+                <div className="dashboard-barber-info flex-column flex-center mini-margin">
+                  <div className="dashboard-barber-first">{barber.firstName}</div>
+                  <div className="dashboard-barber-last">{barber.lastName}</div>
+                </div>
+              </div>
+            )
+          })}
+      </div>
     </div>
   );
 };
